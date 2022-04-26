4b7ce1dbde9b1a4d1656dcb96c3ca9c2875b7a25
Susan Minott <susan_minott@yahoo.com>
2022-04-25 23:34:40 -0400

    commit tree 1ad8e767c2b5765ba06f37ab915c0cd84263e7bb

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 10;
+const num4 = 6;
+const mod = num3 % 6;
+
+const max = Math.max(5, 10, 15, 20);
